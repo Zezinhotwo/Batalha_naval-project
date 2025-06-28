e0e849617ff352d3b0c3198a7dc0c2ba1a1e0cec
zezinhotwo <107373132+Zezinhotwo@users.noreply.github.com>
2025-06-27 22:08:54 -0300

    intalada dependencias para tdd

---
+++ src/scripts/board.js
@@ -0,0 +1,16 @@
+class Borad {
+    constructor() {
+
+    }
+    matriz() {
+        const player = Array.from(Array(100).keys()).fill(0)
+        return player;
+    }
+    addNavio(current){
+        
+    }
+
+}
+
+const play1 = new Borad();
+const play2 = new Borad();
